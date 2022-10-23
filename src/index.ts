95fa348d3c1d0547e9027622df0904f5bfbb7dcf
Naitik Shah <n@daaku.org>
2022-10-23 13:18:24 +0400

    leaner prettier style

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,40 @@
-import { parse } from 'regexparam';
+import { parse } from 'regexparam'
 
 export interface Handler {
-  (params: Record<string, string>, hash: string): void;
+  (params: Record<string, string>, hash: string): void
 }
 
 export interface RouterOptions {
-  on404?: Handler;
+  on404?: Handler
 }
 
 export interface GoOptions {
-  replace?: boolean;
-  title?: string;
-  data?: unknown;
-  force?: boolean;
+  replace?: boolean
+  title?: string
+  data?: unknown
+  force?: boolean
 }
 
 interface Route {
-  handler: Handler;
-  keys: string[];
-  pattern: RegExp;
+  handler: Handler
+  keys: string[]
+  pattern: RegExp
 }
 
 const validHash = (hash: string): void => {
   if (!hash.startsWith('#/')) {
-    throw new Error('hash must begin with #/');
+    throw new Error('hash must begin with #/')
   }
-};
+}
 
 // Router once mounted will listen to history events and dispatch them to the
 // registered handlers.
 export default class Router {
-  private on404?: Handler;
-  private routes: Route[] = [];
+  private on404?: Handler
+  private routes: Route[] = []
 
   constructor({ on404 }: RouterOptions = {}) {
-    this.on404 = on404;
+    this.on404 = on404
   }
 
   // Navigates to the specified hash. This is a minor convenience over the built
@@ -44,53 +44,53 @@ export default class Router {
     { replace, title, data, force }: GoOptions = { replace: false },
   ): void {
     if (!hash) {
-      hash = '#/';
+      hash = '#/'
     }
-    validHash(hash);
+    validHash(hash)
     if (!force && location.hash === hash) {
-      return;
+      return
     }
-    const method = replace ? 'replaceState' : 'pushState';
-    history[method](data, title ?? document.title, hash);
-    this.dispatch(hash);
+    const method = replace ? 'replaceState' : 'pushState'
+    history[method](data, title ?? document.title, hash)
+    this.dispatch(hash)
   }
 
   // Register a pattern and the corresponding handler.
   on(pattern: string, handler: Handler): this {
-    validHash(pattern);
-    pattern = pattern.substring(1); // drop leading # for regexparam
+    validHash(pattern)
+    pattern = pattern.substring(1) // drop leading # for regexparam
     this.routes.push({
       ...parse(pattern),
       handler,
-    });
-    return this;
+    })
+    return this
   }
 
   // Dispatch the handler for the given hash (or the current one if
   // unspecified).
   dispatch(hash = location.hash): void {
-    validHash(hash);
-    const original = hash;
-    const qmark = hash.indexOf('?');
+    validHash(hash)
+    const original = hash
+    const qmark = hash.indexOf('?')
     if (qmark > -1) {
-      hash = hash.slice(0, qmark);
+      hash = hash.slice(0, qmark)
     }
-    hash = hash.substring(1); // drop leading # for regexparam
+    hash = hash.substring(1) // drop leading # for regexparam
     for (let i = 0; i < this.routes.length; i++) {
-      const route = this.routes[i];
-      const matches = route.pattern.exec(hash);
+      const route = this.routes[i]
+      const matches = route.pattern.exec(hash)
       if (!matches) {
-        continue;
+        continue
       }
-      const params: Record<string, string> = {};
+      const params: Record<string, string> = {}
       route.keys.forEach((name, idx) => {
-        params[name] = matches[idx + 1];
-      });
-      route.handler(params, original);
-      return;
+        params[name] = matches[idx + 1]
+      })
+      route.handler(params, original)
+      return
     }
     if (this.on404) {
-      this.on404({}, original);
+      this.on404({}, original)
     }
   }
 
@@ -98,15 +98,15 @@ export default class Router {
   // dispatch to the given hash, the current hash if one is set, or falling back
   // to #/. Returns a function that can be invoked to unmount the Router.
   mount(hash = location.hash): { (): void } {
-    const run = () => this.dispatch();
-    window.addEventListener('popstate', run);
-    window.addEventListener('pushstate', run);
-    window.addEventListener('replacestate', run);
-    this.go(hash, { replace: true, force: true });
+    const run = () => this.dispatch()
+    window.addEventListener('popstate', run)
+    window.addEventListener('pushstate', run)
+    window.addEventListener('replacestate', run)
+    this.go(hash, { replace: true, force: true })
     return () => {
-      window.removeEventListener('popstate', run);
-      window.removeEventListener('pushstate', run);
-      window.removeEventListener('replacestate', run);
-    };
+      window.removeEventListener('popstate', run)
+      window.removeEventListener('pushstate', run)
+      window.removeEventListener('replacestate', run)
+    }
   }
 }
--- test/tests.ts
+++ test/tests.ts
@@ -1,49 +1,49 @@
-import Router from '../src/index.js';
+import Router from '../src/index.js'
 
-QUnit.test('Test It All', async (assert) => {
-  const done = assert.async();
+QUnit.test('Test It All', async assert => {
+  const done = assert.async()
   // eslint-disable-next-line prefer-const
-  let unmount: { (): void };
+  let unmount: { (): void }
   const router = new Router({
     on404: (params, hash) => {
-      document.title = 'Test: 404';
-      assert.deepEqual(params, {}, 'empty params');
-      assert.deepEqual(hash, '#/from-404', 'the unknown path');
-      unmount();
-      location.hash = '';
-      done();
+      document.title = 'Test: 404'
+      assert.deepEqual(params, {}, 'empty params')
+      assert.deepEqual(hash, '#/from-404', 'the unknown path')
+      unmount()
+      location.hash = ''
+      done()
     },
-  });
+  })
   router.on('#/', (params, hash) => {
-    document.title = 'Test: Home';
-    assert.deepEqual(params, {}, 'empty params');
-    assert.deepEqual(hash, '#/', 'the root path');
-  });
+    document.title = 'Test: Home'
+    assert.deepEqual(params, {}, 'empty params')
+    assert.deepEqual(hash, '#/', 'the root path')
+  })
   router.on('#/from-a-click/:answer', (params, hash) => {
-    document.title = 'Test: Click';
-    assert.deepEqual(params, { answer: '42' }, 'the answer');
-    assert.deepEqual(hash, '#/from-a-click/42', 'the path from the href');
-    router.go('#/from-go/43');
-  });
+    document.title = 'Test: Click'
+    assert.deepEqual(params, { answer: '42' }, 'the answer')
+    assert.deepEqual(hash, '#/from-a-click/42', 'the path from the href')
+    router.go('#/from-go/43')
+  })
   router.on('#/from-go/:answer', (params, hash) => {
-    document.title = 'Test: Go';
-    assert.deepEqual(params, { answer: '43' }, 'the answer');
-    assert.deepEqual(hash, '#/from-go/43', 'the path from go');
-    router.go('#/from-go-with-query/44?a=1');
-  });
+    document.title = 'Test: Go'
+    assert.deepEqual(params, { answer: '43' }, 'the answer')
+    assert.deepEqual(hash, '#/from-go/43', 'the path from go')
+    router.go('#/from-go-with-query/44?a=1')
+  })
   router.on('#/from-go-with-query/:answer', (params, hash) => {
-    document.title = 'Test: Go With Query';
-    assert.deepEqual(params, { answer: '44' }, 'the answer');
+    document.title = 'Test: Go With Query'
+    assert.deepEqual(params, { answer: '44' }, 'the answer')
     assert.deepEqual(
       hash,
       '#/from-go-with-query/44?a=1',
       'the path from go with the query',
-    );
+    )
     // this does nothing since we're already here
-    router.go(location.hash);
-    router.go('#/from-404');
-  });
-  unmount = router.mount();
+    router.go(location.hash)
+    router.go('#/from-404')
+  })
+  unmount = router.mount()
   /* eslint-disable @typescript-eslint/no-non-null-assertion */
-  (document.querySelector('#start-it') as HTMLAnchorElement)!.click();
-});
+  ;(document.querySelector('#start-it') as HTMLAnchorElement)!.click()
+})
